a34f19d47b6ccb65bdc50061ea72887946a7b319
taha127 <tahadehghani127@gmail.com>
2026-07-06 18:30:40 +0330

    Remove CalibrationPage integration from AppWindow

--- widgets/app_window.py
+++ widgets/app_window.py
@@ -1,7 +1,6 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QStackedWidget
 
 from widgets.main_window import MainWindow
-from widgets.calibration_page import CalibrationPage
 from services.camera_manager import CameraManager, CameraWidget
 from widgets.camera_select_dialog import CameraSelectDialog
 
@@ -30,27 +29,6 @@ class AppWindow(QWidget):
 
         # Pages
         self.main_page = MainWindow(self.camera_widget)
-        self.calibration_page = CalibrationPage(self.camera_widget)
-
         self.stack.addWidget(self.main_page)
-        self.stack.addWidget(self.calibration_page)
 
-        self.stack.setCurrentWidget(self.main_page)
-
-        # Navigation
-        self.main_page.go_calibration.connect(self.show_calibration)
-        self.calibration_page.profileSelected.connect(self.show_detection)
-
-        self.calibration_page.profileSelected.connect(
-            self.main_page.set_profile)
-
-    # go to calibration page
-    def show_calibration(self):
-
-        self.stack.setCurrentWidget(self.calibration_page)
-
-    # come back to main page with Profile
-    def show_detection(self, profile):
-
-        self.main_page.ui.NameOfProfile.setText(profile["name"])
         self.stack.setCurrentWidget(self.main_page)
