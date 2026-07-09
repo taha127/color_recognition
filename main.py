9ba97b200d29fbac64038ea0aab60a0bf9b267f0
taha127 <tahadehghani127@gmail.com>
2026-07-09 19:09:59 +0330

    Add license activation check in main function

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from PySide6.QtGui import QIcon
-
+from PySide6.QtWidgets import QDialog
 import resources.resources_rc
 from widgets.app_window import AppWindow
+from widgets.activate_license_dialog import ActivateLicenseDialog
+from services.license_manager import LicenseManager
 
 
 def main():
@@ -11,6 +13,15 @@ def main():
 
     app.setWindowIcon(QIcon(":icons/favicon256.ico"))
     app.setApplicationName("Color Recognition App")
+    license_manager = LicenseManager()
+    if not license_manager.can_run():
+
+        dlg = ActivateLicenseDialog()
+        if dlg.exec() != QDialog.DialogCode.Accepted:
+            sys.exit(0)
+
+        if not license_manager.can_run():
+            sys.exit(0)
 
     window = AppWindow()
     window.showMaximized()
