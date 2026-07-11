b40e2045588b6dbc4248925b9624d284880dc5f6
taha127 <tahadehghani127@gmail.com>
2026-07-11 10:10:18 +0330

    Enhance color loading logic to validate hex codes and remove invalid entries

--- services/color_history_srv.py
+++ services/color_history_srv.py
@@ -2,6 +2,7 @@ import json
 import datetime
 import os
 from services.app_paths import get_app_data_dir
+from PySide6.QtGui import QColor
 
 
 class ColorHistoryService:
@@ -25,13 +26,32 @@ class ColorHistoryService:
 
         try:
             with open(self.saved_colors_file_path, "r", encoding="utf-8") as f:
-                return json.load(f)
+                colors = json.load(f)
 
         except (json.JSONDecodeError, IOError) as e:
             return []
 
+        valid_colors = []
+        modified = False
+
+        for color in colors:
+            hex_code = color.get("hex", "").strip()
+            if not QColor(hex_code).isValid():
+                modified = True
+                continue
+
+            valid_colors.append(color)
+
+        if modified:
+            with open(self.saved_colors_file_path, "w", encoding="utf-8") as f:
+                json.dump(valid_colors, f, indent=4, ensure_ascii=False)
+
+        return valid_colors
+
     def save_color(self, name: str, hex_code: str):
 
+        if not QColor(hex_code).isValid():
+            raise ValueError("Invalid color")
         colors = self.load_saved_colors()
 
         new_entry = {
@@ -49,7 +69,6 @@ class ColorHistoryService:
     def delete_color_by_name(self, name: str):
 
         colors = self.load_saved_colors()
-        print(name)
         colors = [c for c in colors if c["name"] != name]
 
         with open(self.saved_colors_file_path, "w", encoding="utf-8") as f:
